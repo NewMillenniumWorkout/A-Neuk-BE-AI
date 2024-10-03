94a919f3de5f207d77e1b2643f4f8d753cae4f78
michang <minseok128128@gmail.com>
2024-10-03 22:07:59 +0900

    Added main.py : docs 연습

---
+++ main.py
@@ -0,0 +1,38 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+class ItemResponse(BaseModel):
+    item_id: int
+    item_name: str
+    item_price: float
+    item_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}", response_model=ItemResponse)
+def update_item(item_id: int, item: Item):
+    return ItemResponse(
+        item_id=item_id,
+        item_name=item.name,
+        item_price=item.price,
+        item_offer=item.is_offer,
+    )
