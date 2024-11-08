64efdffa047df47b163ba340692f7d53d9fc50f7
michang <minseok128128@gmail.com>
2024-11-08 20:30:50 +0900

    Update main.py : load_dotenv를 가장 최초에 실행 한 뒤 각 모듈 import

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI
 from dotenv import load_dotenv
+from fastapi import FastAPI
+
+load_dotenv()
+
 from srcs.auth import get_protected_docs
 from srcs.routes import root
 from srcs.routes import test
 from srcs.routes import chat
 
-
 app = FastAPI()
-load_dotenv()
 get_protected_docs(app)
 
 app.include_router(root.router)
