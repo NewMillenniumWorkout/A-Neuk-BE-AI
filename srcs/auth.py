5cc70443355ea7d569e76e28408515990a756c25
michang <minseok128128@gmail.com>
2024-11-08 15:03:50 +0900

    Update auth.py : formatting & legacy 삭제

--- srcs/auth.py
+++ srcs/auth.py
@@ -4,10 +4,8 @@ from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
 from starlette.responses import JSONResponse
 import secrets
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
 security = HTTPBasic()
 username = os.getenv("SWAGGER_USERNAME")
