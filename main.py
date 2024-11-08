c554e1dbe736a65489a7d28282bf8851e03dab04
michang <minseok128128@gmail.com>
2024-11-08 17:17:42 +0900

    Update main.py : chat 라우팅

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from srcs.auth import get_protected_docs
 from srcs.routes import root
 from srcs.routes import test
+from srcs.routes import chat
 
 
 app = FastAPI()
@@ -11,3 +12,4 @@ get_protected_docs(app)
 
 app.include_router(root.router)
 app.include_router(test.router)
+app.include_router(chat.router)
