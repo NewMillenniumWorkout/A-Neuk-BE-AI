8175982439fce350c49f429e4970b97aa8d9c980
michang <minseok128128@gmail.com>
2024-11-08 15:03:38 +0900

    Update main.py : dotenv load

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
 from srcs.auth import get_protected_docs
 from srcs.routes import root
 from srcs.routes import test
 
+
 app = FastAPI()
+load_dotenv()
 get_protected_docs(app)
 
 app.include_router(root.router)
