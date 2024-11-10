d71526ed251040f086de4562f692b6e8514a14d6
michang <minseok128128@gmail.com>
2024-11-10 15:10:25 +0900

    Added diary.py : core 함수 체인으로 활용하여 결과물 return 테스트 성공

---
+++ app/routes/diary.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+from app.models.diary_models import DiaryRequest, DiaryResponse
+from app.ai_cores.diary_cores import diary_generate, diary_split, diary_find_emotions
+
+
+router = APIRouter(prefix="/ai/diary", tags=["Diary"])
+
+
+@router.post("/", response_model=DiaryResponse)
+def chat_post(request: DiaryRequest):
+    diary_original = diary_generate(request)
+    diary_splitted = diary_split(diary_original)
+    diary_emotions = diary_find_emotions(diary_splitted)
+
+    return DiaryResponse(chat_id=request.chat_id, content_list=diary_emotions)
