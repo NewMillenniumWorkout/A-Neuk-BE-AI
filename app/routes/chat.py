bb96767eb31d86e7eca6d9f3b7030c3543ab3b8c
michang <minseok128128@gmail.com>
2024-11-10 15:09:54 +0900

    Update chat.py : API 명세에 맞게 chat_id도 응답에 포함

--- app/routes/chat.py
+++ app/routes/chat.py
@@ -9,6 +9,7 @@ router = APIRouter(prefix="/ai/chat", tags=["Chat"])
 @router.post("/", response_model=ChatResponse)
 def chat_post(request: ChatRequest):
     return ChatResponse(
+        chat_id=request.chat_id,
         role="ASSISTANT",
         message= chat_generate(request)
     )
