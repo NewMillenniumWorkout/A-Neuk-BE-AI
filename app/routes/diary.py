26666c4ff67c153c0239ef88859fb415cf367263
michang <minseok128128@gmail.com>
2024-11-10 16:20:53 +0900

    Update diary.py :  try-except를 통해 엔드포인트 예외처리

--- app/routes/diary.py
+++ app/routes/diary.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from app.models.diary_models import DiaryRequest, DiaryResponse
+from fastapi import APIRouter, HTTPException
+from app.models.diary_models import DiaryRequest, DiaryResponse, LLMError
 from app.ai_cores import diary_generate, diary_split, diary_find_emotions
 
 
@@ -9,17 +9,24 @@ router = APIRouter(prefix="/ai/diary", tags=["Diary"])
 def debug_process(original, splitted, emotions):
     print("---Original---\n", original)
     print("\n")
-    print("---Splitted---\n", splitted)
+    print("---Splitted---")
+    for idx, content in enumerate(splitted):
+        print(f"{idx}. {content}")
     print("\n")
-    print("---Emotions---\n", emotions)
+    print("---Emotions---")
+    for idx, content in enumerate(emotions):
+        print(f"{idx}. {content.recommend_emotion}")
     print("\n")
 
 
 @router.post("/", response_model=DiaryResponse)
-def chat_post(request: DiaryRequest):
-    original = diary_generate(request)
-    splitted = diary_split(original)
-    emotions = diary_find_emotions(splitted)
-    debug_process(original, splitted, emotions)
+def diary_post(request: DiaryRequest):
+    try:
+        original = diary_generate(request)
+        splitted = diary_split(original)
+        emotions = diary_find_emotions(splitted)
+        debug_process(original, splitted, emotions)
+    except LLMError as e:
+        raise HTTPException(status_code=500, detail=f"Diary generation failed: {e}")
 
     return DiaryResponse(chat_id=request.chat_id, content_list=emotions)
