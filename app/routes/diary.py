1fd7bd251219429afa58a92fdfd6e353bc0e1442
michang <minseok128128@gmail.com>
2024-11-10 15:52:43 +0900

    Update diary.py : 결과 도출 과정 디버깅용 debug_process 함수 추가

--- app/routes/diary.py
+++ app/routes/diary.py
@@ -6,10 +6,20 @@ from app.ai_cores import diary_generate, diary_split, diary_find_emotions
 router = APIRouter(prefix="/ai/diary", tags=["Diary"])
 
 
+def debug_process(original, splitted, emotions):
+    print("---Original---\n", original)
+    print("\n")
+    print("---Splitted---\n", splitted)
+    print("\n")
+    print("---Emotions---\n", emotions)
+    print("\n")
+
+
 @router.post("/", response_model=DiaryResponse)
 def chat_post(request: DiaryRequest):
-    diary_original = diary_generate(request)
-    diary_splitted = diary_split(diary_original)
-    diary_emotions = diary_find_emotions(diary_splitted)
+    original = diary_generate(request)
+    splitted = diary_split(original)
+    emotions = diary_find_emotions(splitted)
+    debug_process(original, splitted, emotions)
 
-    return DiaryResponse(chat_id=request.chat_id, content_list=diary_emotions)
+    return DiaryResponse(chat_id=request.chat_id, content_list=emotions)
