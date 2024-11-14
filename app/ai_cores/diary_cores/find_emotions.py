08d79b312a560b16bb5eec863d3582d54f2f104e
michang <minseok128128@gmail.com>
2024-11-14 16:42:06 +0900

    Update find_emotions.py : 코드 정리

--- app/ai_cores/diary_cores/find_emotions.py
+++ app/ai_cores/diary_cores/find_emotions.py
@@ -1,31 +1,13 @@
+import os
+import random
+import faiss
+import numpy as np
 from typing import List
-
-from app.models.diary_models import DiaryContent
-from pydantic import BaseModel
+from app.models.diary_models import DiaryContent, DiaryEmotionList
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
-import random
-import dotenv
-import faiss
-import numpy as np
-import os
 from langchain.embeddings import OpenAIEmbeddings
-
-dotenv.load_dotenv()
-
-
-class DiaryEmotionList(BaseModel):
-    common_top1: str
-    common_top2: str
-    common_top3: str
-    common_top4: str
-    common_top5: str
-    uncommon_top1: str
-    uncommon_top2: str
-    uncommon_top3: str
-    uncommon_top4: str
-    uncommon_top5: str
 
 
 with open("emotions.txt", "r", encoding="utf-8") as f:
@@ -37,6 +19,27 @@ embedding_model = OpenAIEmbeddings(model="text-embedding-3-large")
 output_parser = JsonOutputParser(pydantic_object=DiaryEmotionList)
 format_instructions = output_parser.get_format_instructions()
 
+system_prompt = """
+The input sentence is part of a diary entry describing the user’s day. Please perform the following tasks to identify emotions the diary author may have felt but might not have fully noticed.
+
+1. **Extract a total of 10 possible emotions** that the author may have felt in this context, taking into account any **emotional shifts or changes** within the sentence (e.g., if there’s an emotional transition, list the relevant emotions for each shift).
+2. Classify the emotions into two categories:
+   - **common_top1~5**: 5 **common emotions** likely felt in response to the diary’s context. These are emotions that are predictable or easily inferred from the situation.
+   - **uncommon_top1~5**: 5 **uncommon emotions** (specific or unique feelings) that may have been felt. These emotions are less expected in the context but could represent inner thoughts or subtle nuances.
+3. Ensure that **no emotions already present in the input sentence** are selected.
+4. Consider any **overlapping emotions** within the same situation or event in the input (e.g., a mix of enjoyment with a hint of anxiety).
+5. Select all emotions from the provided **list of 421 Korean emotional words**.
+6. **Ensure the chosen emotions feel natural and contextually fitting** within the diary entry. Review each emotion to confirm its relevance.
+"""
+
+prompt = PromptTemplate(
+    template="{prompt}\n{format_instructions}\nKorean emotional words:{emotion_list_with_comma}\nUser input:{query}\n",
+    input_variables=["query", "emotion_list_with_comma"],
+    partial_variables={
+        "prompt": system_prompt,
+        "format_instructions": format_instructions,
+    },
+)
 
 # FAISS 인덱스 및 레이블 파일이 이미 존재하는지 확인
 if os.path.exists("emotion_embeddings.index") and os.path.exists("emotion_labels.npy"):
@@ -61,28 +64,6 @@ else:
     with open("emotion_labels.npy", "wb") as f:
         np.save(f, np.array(emotions_db))
 
-system_prompt = """
-The input sentence is part of a diary entry describing the user’s day. Please perform the following tasks to identify emotions the diary author may have felt but might not have fully noticed.
-
-1. **Extract a total of 10 possible emotions** that the author may have felt in this context, taking into account any **emotional shifts or changes** within the sentence (e.g., if there’s an emotional transition, list the relevant emotions for each shift).
-2. Classify the emotions into two categories:
-   - **common_top1~5**: 5 **common emotions** likely felt in response to the diary’s context. These are emotions that are predictable or easily inferred from the situation.
-   - **uncommon_top1~5**: 5 **uncommon emotions** (specific or unique feelings) that may have been felt. These emotions are less expected in the context but could represent inner thoughts or subtle nuances.
-3. Ensure that **no emotions already present in the input sentence** are selected.
-4. Consider any **overlapping emotions** within the same situation or event in the input (e.g., a mix of enjoyment with a hint of anxiety).
-5. Select all emotions from the provided **list of 421 Korean emotional words**.
-6. **Ensure the chosen emotions feel natural and contextually fitting** within the diary entry. Review each emotion to confirm its relevance.
-"""
-
-prompt = PromptTemplate(
-    template="{prompt}\n{format_instructions}\nKorean emotional words:{emotion_list_with_comma}\nUser input:{query}\n",
-    input_variables=["query", "emotion_list_with_comma"],
-    partial_variables={
-        "prompt": system_prompt,
-        "format_instructions": format_instructions,
-    },
-)
-
 
 def find_most_similar_word_embedding(target_word, top_n=1):
     input_embedding = np.array(
@@ -98,12 +79,6 @@ def find_most_similar_word_embedding(target_word, top_n=1):
     return top_emotions[0]
 
 
-def check_valid_emotion(emotion: str) -> bool:
-    if emotion in emotions_db:
-        return True
-    return False
-
-
 async def find_emotions_from_sentence(sentence: str) -> List[str]:
     random.shuffle(emotions_db)
     chain = prompt | llm | output_parser
@@ -113,10 +88,10 @@ async def find_emotions_from_sentence(sentence: str) -> List[str]:
     result_list = []
     for key, value in result.items():
         if key.startswith("common") or key.startswith("uncommon"):
-            if check_valid_emotion(value):
+            if value in emotions_db:
                 result_list.append(value)
             else:
-                # result_list.append(find_most_similar_word_embedding(value))
+                # result_list.append(find_most_similar_word_embedding(value)) # 임베딩 기반 검색
                 pass
     return result_list
 
